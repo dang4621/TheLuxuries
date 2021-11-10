e88f3ca939aee7f75c1f4f7fb97f27aa4b831963
danghhps17869 <86336577+danghhps17869@users.noreply.github.com>
2021-11-10 18:06:25 +0700

    add san pham

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,16 @@
 <?php 
-    
+    session_start();
     include '../model/pdo.php';
-    include '../model/danhmuc.php';
-    
+    include '../model/sanpham.php';   
+    include '../model/danhmuc.php';    
     include '../model/thuonghieu.php';
     
 
     include 'header.php';
-
+    $danhmuc = loadAll_dm();
     if( isset($_GET['act']) ){
         $act=$_GET['act'];
-
+        
         switch($act){
             //Danh mục
             case 'add_dm':
@@ -22,7 +22,6 @@
                 }     
                 break;
             case 'dsdanhmuc':
-                $danhmuc = loadAll_dm();
                 include '../admin/danhmuc/dsdanhmuc.php';   
                 break;
             case 'edit_dm':
@@ -39,7 +38,6 @@
                     update_dm($ten_nhom_hang,$ma_nhom_hang);
                     echo ('<script>alert("Cập nhật thành công")</script>');
                 }
-                $danhmuc = loadAll_dm();
                 include '../admin/danhmuc/dsdanhmuc.php';
                 break;
             case 'del_dm':
@@ -47,7 +45,6 @@
                     $id=$_GET['id'];
                     del_dm($id);
                 }
-                $danhmuc = loadAll_dm();
                 include '../admin/danhmuc/dsdanhmuc.php';      
                 break; 
             //Thương hiệu
@@ -60,27 +57,73 @@
                 include '../admin/thuonghieu/thuonghieu.php';
                 
                 if(isset($_POST['add'])){
-         $tenth=$_POST['name'];
-         $xuatxu=$_POST['xuatxu'];
-         $filename=$_FILES['img']['name'];
-         $target_dir = "../upload/";
-         $target_file = $target_dir . basename($_FILES["img"]["name"]);
-
-         themthuonghieu($tenth,$xuatxu,$filename);
-
-    
-   echo ('<script>alert("Cập nhật thành công")</script>');
-}
+                $tenth=$_POST['name'];
+                $xuatxu=$_POST['xuatxu'];
+                $filename=$_FILES['img']['name'];
+                $target_dir = "../upload/";
+                $target_file = $target_dir . basename($_FILES["img"]["name"]);
+                themthuonghieu($tenth,$xuatxu,$filename);                    
+                echo ('<script>alert("Cập nhật thành công")</script>');
+                }
                 break;
 
             //Sản phẩm
-            case 'add_sp':
-                $danhmuc = loadAll_dm();
+            case 'add_sp': 
+                include '../admin/sanpham/sanpham.php';                
                 if(isset($_POST['submit'])){
+                    $ma_san_pham = rand(1000000000,9999999999);
+                    $ma_nhom_hang=$_POST['id_dm'];
+                    $ma_thuong_hieu = 4231423412;
+                    $ten_san_pham=$_POST['ten_sp'];   
 
-                       
+                     //upload nhiều ảnh
+                     $targetDir = "../upload/"; 
+                     $allowTypes = array('jpg','png','jpeg','gif'); 
+                     $fileNames = $_FILES['anhsp']['name']; 
+                     if(!empty($fileNames)){                         
+                         $file = implode(',', $fileNames);
+                         // print_r($file);
+                         foreach($_FILES['anhsp']['name'] as $key=>$val){              
+                             // địa chỉ của file 
+                             $fileName = basename($_FILES['anhsp']['name'][$key]); 
+                             $targetFilePath = $targetDir . $fileName; 
+                             // kiểm tra định dạng file đúng là ảnh hay không 
+                             $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+                             if(in_array($fileType, $allowTypes)){ 
+                                     move_uploaded_file($_FILES["anhsp"]["tmp_name"][$key], $targetFilePath);
+                             }else{ 
+                                 $mes="Tải tệp thất bại";
+                             } 
+                         } 
+                        
+                     }else{ 
+                        $mes="Tải tệp thất bại";
+                     } 
+
+
+                    $gia_goc=$_POST['gia_goc'];
+                    $giam_gia=$_POST['giam_gia'];
+                    $timestamp = time();
+                    $today=date("20y-m-d h:i:s", $timestamp);
+                    $mo_ta=$_POST['mota'];
+                    themSanpham($ma_san_pham,$ma_nhom_hang,$ma_thuong_hieu,$ten_san_pham,$file, $gia_goc,$giam_gia,$today, $mo_ta); 
+                    include '../admin/sanpham/thuoctinh.php';  
                 }
-                include '../admin/sanpham/sanpham.php'; 
+                
+                
+                break;
+            case 'thuoctinh':
+                if(isset($_SESSION['thuoctinh'])){
+                    $ma_san_pham=$_POST['ma_san_pham'];
+                    foreach($_SESSION["thuoctinh"] as $value){
+                    extract($value);      
+                     themThuocTinh($ma_san_pham,$size,$color,$quantity);
+                    }
+                    unset($_SESSION['thuoctinh']);
+                }
+                break;
+            case 'add_tt':
+                include '../admin/sanpham/thuoctinh.php';  
                 break;
             case 'dssanpham':
                 
---
+++ admin/sanpham/thuoctinh.php
@@ -0,0 +1,48 @@
+<div class="contact-from-section mt-150 mb-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 mb-5 mb-lg-0">
+                       <form method="POST"> 
+							<p>Thêm các loại sản phẩm và số lượng của từng loại</p>
+								<input name="size" type="radio" id="html"  value="S">
+									<label for="html">S</label>
+								<input name="size" type="radio" id="css"  value="M">
+									<label for="css">M</label>
+								<input name="size" type="radio" id="javascript"  value="L">
+									<label for="javascript">L</label>
+								<input name="size" type="radio" id="javascript"  value="XL">
+									<label for="javascript">XL</label>
+								<input name="size" type="radio" id="javascript"  value="XXL">
+									<label for="javascript">XXL</label><br>
+
+									<label for="favcolor">Chọn màu hiển thị</label>
+								<input type="color" id="favcolor" name="favcolor" value="#ff0000"><br><br>
+									<label for="">Số lượng</label>
+								<input id="number" name="number" type="number" value="50"> <br>
+								<button type="submit" name="add_session">Thêm loại</button>
+							</form>			
+                            <!-- <a href="index.php?act=thuoctinh">Chấp nhận</a>				 -->
+                            <form action="index.php?act=thuoctinh" method="POST">
+                                <input type="hidden" name="ma_san_pham" value="<?= $ma_san_pham ?>">
+                                <input type="submit" name="themthuoctinh">
+                            </form>
+								<?php             
+									 if(isset($_POST['add_session'])){                   
+										$row = ["size" => $_POST['size'] ,"color" =>  $_POST['favcolor'] ,"quantity" =>  $_POST['number'] ];              
+										$_SESSION["thuoctinh"][$_POST['size'].$_POST['favcolor'].$_POST['number']]=$row;
+									}  
+									if(isset($_SESSION['thuoctinh'])){
+										foreach($_SESSION["thuoctinh"] as $value){
+										extract($value);   
+										echo("Size: ".$size." Màu : ".$color." Số lượng : ".$quantity."");
+										echo("<br>");
+										} 
+									}else{
+										echo("KO CÓ GÌ");
+									} 
+									echo("<pre>");
+							?>
+				</div>
+			</div>
+		</div>
+	</div>
