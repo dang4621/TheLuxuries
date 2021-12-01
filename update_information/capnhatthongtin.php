ee3491bfcd1bb0da7e5f91ab22bb360b4133f4bc
congdinh212 <86062266+congdinh212@users.noreply.github.com>
2021-12-01 16:57:33 +0700

    update capnhatthongtin

---
+++ update_information/capnhatthongtin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Cập nhật thông tin</title>
+	<!-- fonts -->
+	<link href="//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+	<!-- /fonts -->
+	<!-- css -->
+	<link href="../view/assets/css/capnhatthongtin.css" rel='stylesheet' type='text/css' media="all" />
+	<!-- /css -->
+</head>
+
+<body>
+	<div class="capnhatthongtin">
+		<h1 class="w3ls">Cập nhật thông tin</h1>
+		<div class="content-w3ls">
+			<div class="content-agile1">
+				<h2 class="agileits1">The Luxuries</h2>
+				<p class="agileits2">THỜI TRANG LUÔN THAY ĐỔI</p>
+			</div>
+			<div class="content-agile2">
+				<form action="#" method="post">
+					<div class="form-control ">
+						<input type="text" id="firstname" name="firstname" placeholder="Tên người dùng" required="">
+					</div>
+
+					<div class="form-control ">
+						<input type="text" id="hoten" name="hoten" placeholder="Họ tên" required="">
+					</div>
+
+					<div class="form-control ">
+						<input type="email" id="email" name="email" placeholder="Email" required="">
+					</div>
+
+					<div class="form-control ">
+						<input type="number" id="number" name="number" placeholder="Số điện thoại" min="0" required="">
+					</div>
+
+					<div class="form-control agileinfo">
+						<input type="password" class="lock" name="password" placeholder="Mật khẩu" id="password1" required="">
+					</div>
+
+					<div class="form-control agileinfo">
+						<input type="password" class="lock" name="confirm-password" placeholder="Nhập lại mật khẩu" id="password2" required="">
+					</div>
+
+					<input type="submit" class="register" value="Cập nhật">
+				</form>
+			</div>
+			<div class="clear"></div>
+		</div>
+	</div>
+</body>
+
+</html>
