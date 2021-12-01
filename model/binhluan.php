4d1a2d4a478888420c3bfd28040ba9ea8f186a35
danghhps17869 <86336577+danghhps17869@users.noreply.github.com>
2021-12-01 08:49:19 +0700

    add comment

--- model/binhluan.php
+++ model/binhluan.php
@@ -4,4 +4,14 @@ function themBinhLuan($id_tai_khoan,$username,$ma_san_pham,$noi_dung,$ngay_bl){
     ('$id_tai_khoan','$username','$ma_san_pham','$noi_dung','$ngay_bl')";
     pdo_execute($sql);
 }
+function layBL($ma_san_pham){
+    $sql="SELECT tai_khoan.Hoten,binh_luan.noi_dung,binh_luan.ngay_bl              
+            FROM(binh_luan INNER JOIN tai_khoan ON binh_luan.id_tai_khoan=tai_khoan.id_tai_khoan) 
+            WHERE binh_luan.ma_san_pham = '$ma_san_pham'";
+    return pdo_query($sql);
+}
+function lay_idtk($username){
+    $sql="SELECT id_tai_khoan FROM tai_khoan WHERE username='$username'";
+    return pdo_query_one($sql);
+}
 ?>
--- view/binhluan/binhluanform.php
+++ view/binhluan/binhluanform.php
@@ -3,6 +3,8 @@ session_start();
 include '../../model/pdo.php';
 include '../../model/binhluan.php';
 $ma_san_pham=$_REQUEST['ma_san_pham'];
+// $lay_idtk=lay_idtk($_SESSION['user']);
+// extract($lay_idtk);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,66 +15,66 @@ $ma_san_pham=$_REQUEST['ma_san_pham'];
     <title>Document</title>
     
     <style>
-.user {
-    font-weight: bold;
-    color: black;
-}
+        .user {
+            font-weight: bold;
+            color: black;
+        }
 
-.time {
-    color: gray;
-}
+        .time {
+            color: gray;
+        }
 
-.userComment {
-    color: #000;
-    margin-right: 20px;
-}
+        .userComment {
+            color: #000;
+            margin-right: 20px;
+        }
 
-.replies .comment {
-    margin-top: 20px;
+        .replies .comment {
+            margin-top: 20px;
 
-}
+        }
 
-.replies {
-    margin-left: 20px;
-}
+        .replies {
+            margin-left: 20px;
+        }
 
-#registerModal input,
-#logInModal input {
-    margin-top: 10px;
-}
-div.stars {
-  width: 270px;
-  display: inline-block;
-}
- 
-input.star { display: none; }
- 
-label.star {
-  padding: 10px;
-  font-size: 36px;
-  color: #444;
-  transition: all .2s;
-}
- 
-input.star:checked ~ label.star:before {
-  content: '\f005';
-  color: #FD4;
-  transition: all .25s;
-}
- 
-input.star-5:checked ~ label.star:before {
-  color: #FE7;
-  text-shadow: 0 0 20px #952;
-}
- 
-input.star-1:checked ~ label.star:before { color: #F62; }
- 
-label.star:hover { transform: rotate(-15deg) scale(1.3); }
- 
-label.star:before {
-  content: '\f006';
-  font-family: FontAwesome;
-}
+        #registerModal input,
+        #logInModal input {
+            margin-top: 10px;
+        }
+        div.stars {
+        width: 270px;
+        display: inline-block;
+        }
+        
+        input.star { display: none; }
+        
+        label.star {
+        padding: 10px;
+        font-size: 36px;
+        color: #444;
+        transition: all .2s;
+        }
+        
+        input.star:checked ~ label.star:before {
+        content: '\f005';
+        color: #FD4;
+        transition: all .25s;
+        }
+        
+        input.star-5:checked ~ label.star:before {
+        color: #FE7;
+        text-shadow: 0 0 20px #952;
+        }
+        
+        input.star-1:checked ~ label.star:before { color: #F62; }
+        
+        label.star:hover { transform: rotate(-15deg) scale(1.3); }
+        
+        label.star:before {
+        content: '\f006';
+        font-family: FontAwesome;
+        }
 </style>
 </head>
 <body>
@@ -90,20 +92,27 @@ label.star:before {
         <div class="col-md-12">
             <div class="userComments">
                 <div class="comment">
-                <?php
-                echo "noi dung binh luan : ".$ma_san_pham;
-                    //     foreach ($dsbl as $bl) {
-                    // echo'<div class="user">'.$id_tai_khoan.'<span class="time">'.$ngay_bl.'</span></div>'
-                    // echo '<div class="binhluan">'.$noi_dung.'</div><br>'
-                ?>
+                    <?php  $layBL=layBL($ma_san_pham);
+                        foreach ($layBL as $value ){
+                            extract($value); ?>
+                    <div class="user">
+                    <?php if(!empty($Hoten)){
+                            echo($Hoten); 
+                        }else{
+                            echo("ẨN DANH");
+                        }?>
+                    <span class="time"><?= $ngay_bl ?></span>
+                    </div>
+                    <div class="binhluan"><?= $noi_dung ?></div><br>
+                   <?php     }   ?>
                 </div>
             </div>
         </div>
     </div>
     <?php
     if(isset($_POST['guibinhluan'])&&($_POST['guibinhluan'])){
-        $id_tai_khoan=$_POST['id_tai_khoan'];
-        $username=$_SESSION['user'];
+        $id_tai_khoan=$_SESSION['user']['id_tai_khoan'];
+        $username=$_SESSION['user']['username'] ;
         $noi_dung= $_POST['noi_dung'];
         $ma_san_pham=$_POST['ma_san_pham'];
         $ngay_bl=date('h:i:sa d/m/Y'); 
